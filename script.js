4c6e475c8a3b285cc9e0a9231aa3be1b2c00d678
Edwin E <edwin.emilien92@gmail.com>
2022-11-15 18:43:34 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -4,9 +4,11 @@
 // 3)randomly pick 1 of the 3 variables
 // 4) return selected variable 
 
-let choice1 = "Rock"
-let choice2 = "Paper"
-let choice3 = "Scissor"
+let choice1 = "Rock";
+let choice2 = "Paper";
+let choice3 = "Scissor";
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
 
 function getComputerChoice(){
     
@@ -16,9 +18,31 @@ function getComputerChoice(){
         return choice1;
     } 
     
-    if (x > 33 && x <= 66) {
+    else if (x > 33 && x <= 66) {
         return choice2;
-    } else {
+    } 
+    
+    else {
         return choice3;
+    }
+}
+
+// create function playRound 
+// 1)function take in 2 parameter(player choice and computer choice)
+// 2)if statement to return result based on entered information(this 
+//   did not work because all string results return NaN which makes all result falsy)
+
+
+function playRound(playerSelection, computerSelection) {
+    if ( playerSelection == choice3 ){
+        return "I lost! Rock beats scissor";
+    } 
+    
+    else if ( playerSelection == choice2 ) {
+        return "I won! Paper beats rock";
+    } 
+    
+    else {
+        return "Tie! Go again!"
     }
 }
