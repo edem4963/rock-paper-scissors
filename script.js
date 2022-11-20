f1b374471fd6e6ecd2969fdfc50fb949e4b0b42d
Edwin E <edwin.emilien92@gmail.com>
2022-11-20 13:51:54 -0500

    Fix game function

--- script.js
+++ script.js
@@ -72,12 +72,15 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//const playerSelection = prompt("Please enter rock, paper or scisoor: ");
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
 
 function game(){
     for ( let i = 0; i < 5 ; i++ ) {
+        const playerSelection = prompt("Please enter rock, paper or scisoor: ");
+        const computerSelection = getComputerChoice();
+
         console.log(playRound(playerSelection, computerSelection));
     }
 }
